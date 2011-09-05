e6b414f4f92bb1c68daf4d9637932e72b69b3a3e
Alexander <alexander.r.ambrose@gmail.com>
2011-09-05 02:41:53 -0400

    Product Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates_presence_of :title, :description, :image_url
+  validates_uniqueness_of :title
+  validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)$}i, message: 'must be a URL for GIF, JPG or PNG image.' }
 end
