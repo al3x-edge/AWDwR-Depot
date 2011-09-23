56397f6b426be6f89415eb1282bdacc7323e55d4
Alexander <alexander.r.ambrose@gmail.com>
2011-09-22 22:37:22 -0400

    Iteration E3 - Finishing the Cart

--- test/functional/line_items_controller_test.rb
+++ test/functional/line_items_controller_test.rb
@@ -18,7 +18,7 @@ class LineItemsControllerTest < ActionController::TestCase
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post :create, product_id: Fabricate(:product).id
+      post :create, product_id: Fabricate(:product, :title=>"this is longer than 10 characters").id
     end
 
     assert_redirected_to cart_path(assigns(:line_item).cart)
